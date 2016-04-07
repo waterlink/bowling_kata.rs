d0499a52d7c4a4c092e4f5dd251631c0623ae54f
Oleksii Fedorov <waterlink000@gmail.com>
2016-04-07 02:37:34 +0200

    Extract till you drop

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,121 @@ trait BowlingGame {
     fn score(&self) -> i32;
 }
 
+const ALL_BOWLING_PINS: i32 = 10;
+const BOWLING_FRAME_COUNT: usize = 10;
+
+struct PartialScore {
+    frame: Box<Frame>,
+    total: i32
+}
+
+fn empty_partial_score(game: &Game) -> PartialScore {
+    PartialScore{
+        frame: game.create_frame(0),
+        total: 0
+    }
+}
+
+impl PartialScore {
+    fn next_score(&self, game: &Game) -> PartialScore {
+        PartialScore{
+            frame: self.frame.next_frame(game),
+            total: self.total + self.frame.scoreWithBonus(game)
+        }
+    }
+}
+
+trait Frame {
+    fn score(&self, game: &Game) -> i32;
+    fn bonus(&self, game: &Game) -> i32;
+    fn next_frame_index(&self) -> usize;
+
+    fn scoreWithBonus(&self, game: &Game) -> i32 {
+        self.score(game) + self.bonus(game)
+    }
+
+    fn next_frame(&self, game: &Game) -> Box<Frame> {
+        game.create_frame(self.next_frame_index())
+    }
+}
+
+struct NormalFrame {
+    frame_index: usize
+}
+
+struct StrikeFrame {
+    frame_index: usize
+}
+
+struct SpareFrame {
+    frame_index: usize
+}
+
+impl Frame for StrikeFrame {
+    fn score(&self, game: &Game) -> i32 { ALL_BOWLING_PINS }
+    fn bonus(&self, game: &Game) -> i32 { game.strike_bonus(self.frame_index) }
+    fn next_frame_index(&self) -> usize { self.frame_index + 1 }
+}
+
+impl Frame for SpareFrame {
+    fn score(&self, game: &Game) -> i32 { ALL_BOWLING_PINS }
+    fn bonus(&self, game: &Game) -> i32 { game.spare_bonus(self.frame_index) }
+    fn next_frame_index(&self) -> usize { self.frame_index + 2 }
+}
+
+impl Frame for NormalFrame {
+    fn score(&self, game: &Game) -> i32 { game.two_rolls(self.frame_index) }
+    fn bonus(&self, game: &Game) -> i32 { 0 }
+    fn next_frame_index(&self) -> usize { self.frame_index + 2 }
+}
+
 struct Game {
     rolls: Vec<i32>
+}
+
+impl Game {
+    fn two_rolls(&self, frame_index: usize) -> i32 {
+        self.first_roll(frame_index) +
+            self.second_roll(frame_index)
+    }
+
+    fn strike_bonus(&self, strike_index: usize) -> i32 {
+        self.two_rolls(strike_index + 1)
+    }
+
+    fn spare_bonus(&self, spare_index: usize) -> i32 {
+        self.roll_at(spare_index + 2)
+    }
+
+    fn first_roll(&self, index: usize) -> i32 {
+        self.roll_at(index)
+    }
+
+    fn second_roll(&self, index: usize) -> i32 {
+        self.roll_at(index + 1)
+    }
+
+    fn roll_at(&self, index: usize) -> i32 {
+        *(self.rolls.get(index).unwrap_or(&-1))
+    }
+
+    fn is_strike(&self, frame_index: usize) -> bool {
+        self.first_roll(frame_index) == ALL_BOWLING_PINS
+    }
+
+    fn is_spare(&self, frame_index: usize) -> bool {
+        self.two_rolls(frame_index) == ALL_BOWLING_PINS
+    }
+
+    fn create_frame(&self, index: usize) -> Box<Frame> {
+        if self.is_strike(index) {
+            Box::new(StrikeFrame{frame_index: index})
+        } else if self.is_spare(index) {
+            Box::new(SpareFrame{frame_index: index})
+        } else {
+            Box::new(NormalFrame{frame_index: index})
+        }
+    }
 }
 
 fn new_game() -> Game {
@@ -17,35 +130,9 @@ impl BowlingGame for Game {
     }
 
     fn score(&self) -> i32 {
-        let mut total = 0;
-
-        let mut i = 0;
-        for _ in 0..10 {
-            let first_roll = self.rolls[i];
-            i += 1;
-
-            if first_roll == 10 {
-
-                if i + 1 < self.rolls.len() {
-                    total += first_roll + self.rolls[i] + self.rolls[i + 1];
-                }
-
-            } else {
-
-                let second_roll = self.rolls[i];
-                total += first_roll + second_roll;
-                i += 1;
-
-                if first_roll + second_roll == 10 {
-                    if i < self.rolls.len() {
-                        total += self.rolls[i];
-                    }
-                }
-
-            }
-        }
-
-        total
+        (0..BOWLING_FRAME_COUNT).fold(empty_partial_score(self), |partial_score, _| {
+            partial_score.next_score(self)
+        }).total
     }
 }
 
